d975d85ce56bedef77643be05d6a4d76f4c60bbc
hasnain <hasnain@xprolab.com>
2020-03-10 18:18:25 +0500

    Post:Creted new post block&show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,34 @@
 class PostsController < ApplicationController
 	def index
 		#code here
-		
 	end
+	
+	def new
+		@post = Post.new
+	end
+	
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	
+	end
+
+	def show
+		@post = Post.find(params[:id])
+
+	end
+
+
+
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title ,:content)
+	end
+
 end
