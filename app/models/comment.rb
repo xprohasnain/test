8b62a6b0b74cb3fc7c5bd44e1c45058afc213b48
hasnain <hasnain@xprolab.com>
2020-03-16 17:56:47 +0500

    Validations:Excluded words changed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ApplicationRecord
-	 validates :comment, exclusion: { in: %w(www  adult bad nude nudity),
+	validates :comment, exclusion: { in: %w(www  adult bad nude nudity),
     message: "%{value} word is blocked." }
 	validates :name, presence: true 
 	validates :comment, presence: true , length: {minimum: 5},uniqueness: { case_sensitive: false }
+	validates :comment, allow_blank: true
 	validates :comment,inclusion: { in: %w(Good Very_Good medium large)}
 	belongs_to :post
 end
