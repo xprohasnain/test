6eaeaf35dcdbe492526af16c202147916cfd5fef
hasnain <hasnain@xprolab.com>
2020-03-17 17:58:14 +0500

    Models:Updates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@ class Comment < ApplicationRecord
     message: "%{value} word is blocked." }
 	validates :name, presence: true 
 	validates :comment, presence: true , length: {minimum: 5},uniqueness: { case_sensitive: false }
-	validates :comment, allow_blank: true
-	validates :comment,inclusion: { in: %w(Good Very_Good medium large)}
+	# validates :comment,inclusion: { in: %w(Good Very_Good medium large)}
 	belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 	validates :title, presence: true , length: {minimum: 5}
-	validates :content, presence: true , length: {minimum: 5}
+	validates :content, presence: true , length: {minimum: 5},allow_blank: true
 	has_many :comments ,dependent: :destroy
 	validates :content,uniqueness: { case_sensitive: false }
 end
